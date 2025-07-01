22b9bfeac2089d285228d477e5fcfc9cfa6611e0
rust1cean <az.az.az.az.75007500@gmail.com>
2025-07-02 01:19:31 +0500

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { Inter } from "next/font/google";
 import { toast } from "sonner";
+import { getMessages } from "next-intl/server";
 import type { Metadata } from "next";
 
 import "./globals.css";
 import { Header } from "@/widgets/header";
 import { Toaster } from "@/shared/shadcn/shadcn-ui/sonner";
 import { PostDetailsModal } from "@/widgets/post-details-modal";
-import { Providers } from "./providers";
+import { ClientProviders } from "./providers";
 
 const interSans = Inter({
   variable: "--font-inter-sans",
@@ -18,20 +19,25 @@ export const metadata: Metadata = {
   description: "Kidonia Web Application",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }>) {
+  const { locale } = await params;
+  const messages = await getMessages();
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body className={`${interSans.variable} antialiased`}>
-        <Providers>
+        <ClientProviders locale={locale} messages={messages}>
           <Header onNotifyUser={toast} />
           <main className="px-[3vw] lg:px-[10vw] 2xl:px-[25vw] py-[2dvh] md:py-[5dvh]">
             {children}
           </main>
-        </Providers>
+        </ClientProviders>
         <Toaster />
         <PostDetailsModal />
       </body>
